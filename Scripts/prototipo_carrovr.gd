3e661a7b4753219791db7c2dce214adfee9c59a4
luisanyel2001 <69733819+luisanyel2001@users.noreply.github.com>
2024-03-13 22:58:01 -0600

    Señales interseccion

--- Scripts/prototipo_carrovr.gd
+++ Scripts/prototipo_carrovr.gd
@@ -25,18 +25,38 @@ func _carga_nivel(result, response_code, headers, body):
 	json.parse(body.get_string_from_utf8())
 	var response = json.get_data()
 	print(response)
-	
+	print("Entro")
 	#Cargar nombre ciudades y Vincula areas3D de las ci
 	for i in range(1,13):	
 		get_node("Ciudades/LowPolyCITY_" + str(i) + "/Letrero_aereo/Label3D").text = response.ciudades[str(i)]
 		get_node("Ciudades/LowPolyCITY_" + str(i) + "/Letrero_terrestre/Label3D").text = response.ciudades[str(i)]
 		get_node("Ciudades/LowPolyCITY_" + str(i) + "/Area3D").body_entered.connect(func(body):_deteccion_area_ciudad(response.ciudades[str(i)], body))
 	
+	print("Salio")
+	for laberinto in range(1,5):
+		for interseccion in range(1,7):
+			if interseccion != 6:
+				get_node("laberintoTuneles"+str(laberinto)+"/intersection_tunnel_señal"+str(interseccion)+"/Area3D_left").body_entered.connect(_interseccion_salida)
+				get_node("laberintoTuneles"+str(laberinto)+"/intersection_tunnel_señal"+str(interseccion)+"/Area3D_right").body_entered.connect(_interseccion_salida)
+				get_node("laberintoTuneles"+str(laberinto)+"/intersection_tunnel_señal"+str(interseccion)+"/Area3D_center").body_entered.connect(_interseccion_entrada)
+			#Para el caso interseccion izquierda
+			else:
+				get_node("laberintoTuneles"+str(laberinto)+"/intersection_tunnel_señal"+str(interseccion)+"/Area3D_left").body_entered.connect(_interseccion_salida)
+				get_node("laberintoTuneles"+str(laberinto)+"/intersection_tunnel_señal"+str(interseccion)+"/Area3D_right").body_entered.connect(_interseccion_entrada)
+				get_node("laberintoTuneles"+str(laberinto)+"/intersection_tunnel_señal"+str(interseccion)+"/Area3D_center").body_entered.connect(_interseccion_salida)
+			
+			#Recorre los letreros
+			for label in range(1,9):
+				get_node("laberintoTuneles"+str(laberinto)+"/intersection_tunnel_señal"+str(interseccion)+"/doble_sign/left_signs/Label3D" + str(label)).text = "Hola"
+				get_node("laberintoTuneles"+str(laberinto)+"/intersection_tunnel_señal"+str(interseccion)+"/doble_sign/right_signs/Label3D" + str(label)).text = "Hola"
 	
-	for i in range(1,13):	
-		get_node("Ciudades/LowPolyCITY_" + str(i) + "/Letrero_aereo/Label3D").text = response.ciudades[str(i)]
-		get_node("Ciudades/LowPolyCITY_" + str(i) + "/Letrero_terrestre/Label3D").text = response.ciudades[str(i)]
-		get_node("Ciudades/LowPolyCITY_" + str(i) + "/Area3D").body_entered.connect(func(body):_deteccion_area_ciudad(response.ciudades[str(i)], body))
+	print("Termino")
+	
+func _interseccion_entrada(body):
+	print("_interseccion_entrada")
+	
+func _interseccion_salida(body):
+	print("_interseccion_salida")
 		
 		
 func _deteccion_area_ciudad(id, body):
@@ -53,6 +73,7 @@ func _deteccion_area_ciudad(id, body):
 
 
 func _carga_UI(gano):
+	var siguiente_texto = "Guana"
 	if gano:
 		print("Ganaste")
 	else:
