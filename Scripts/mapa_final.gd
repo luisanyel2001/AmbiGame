be6399cb3b1450c2e2df20c04cf811a3f3051394
LuisVDT <luisvital.de@gmail.com>
2024-05-07 07:33:45 -0600

    cosas ia

--- Scripts/mapa_final.gd
+++ Scripts/mapa_final.gd
@@ -147,10 +147,15 @@ func _reinicar():
 		var carro = $player_car as VehicleBody3D
 		carro.linear_velocity = Vector3(0, 0, 0)
 		$player_car.set_global_rotation(Vector3(0, 0, 0))
-
+		
+		
+func _termino():
+	if Global.termino == true:
+		Global.tolerancia = _calculoToleranciaAmbiguedad()
 		
 func _process(delta):
 	_reinicar()
+	_termino()
 	
 	
 func _cargar_siguiente_nivel():
@@ -160,3 +165,98 @@ func _cargar_siguiente_nivel():
 		_peticion_http("https://luisanyel.000webhostapp.com/mapa.json")
 	
 		
+<<<<<<< Updated upstream
+=======
+"""
+# Reposicionar el vehículo al iniciador después del reinicio
+	$player_car.set_global_position(get_node("Ciudades/Ciudad_" + str(ciudades_to_num[Global.ultima_ciudad]) + "/Iniciador").get_global_position())
+	# Reiniciar la velocidad lineal del vehículo a cero
+	var carro = $player_car as VehicleBody3D
+	carro.linear_velocity = Vector3(0, 0, 0)
+	$player_car.set_global_rotation(Vector3(0, 0, 0))
+"""
+func _carga_IA(gano):
+	if gano:		
+		destinosCorrectos += 1
+		_calculoToleranciaAmbiguedad()
+	else:		
+		destinosIncorrectos += 1		
+		_calculoToleranciaAmbiguedad()
+func _calculoToleranciaAmbiguedad():	
+	if destinosCorrectos > 0 || destinosIncorrectos > 0:
+		velocidadPromedio = vehicle.velocidad_promedio / 20 #normalizado sobre la velocidad maxima esperada		
+		var toleranciaAmbiguedad = (destinosCorrectos/(destinosCorrectos+destinosIncorrectos))
+		toleranciaAmbiguedad += velocidadPromedio/tiempoTotal		
+		_guardarCalculoAmbiguedad()		
+		return toleranciaAmbiguedad		
+	else:		
+		return null
+func _guardarCalculoAmbiguedad():		
+	var datos = ConfigFile.new()
+	var resultado = datos.load("historialCalculosAmbiguedad.cfg")	
+	if resultado == ERR_FILE_NOT_FOUND:		
+		datos = ConfigFile.new()  # Crea un nuevo archivo de configuración            
+	# Crea una sección única para cada partida
+	var tiempo_actual = Time.get_date_string_from_system() + "_" + Time.get_time_string_from_system()
+	var seccion = "partida_" + tiempo_actual
+	datos.set_value(seccion, "velocidadPromedio", velocidadPromedio)	
+	datos.set_value(seccion, "tiempoTotal", tiempoTotal)
+	datos.set_value(seccion, "destinosCorrectos", destinosCorrectos)
+	datos.set_value(seccion, "destinosIncorrectos", destinosIncorrectos)
+	# Guarda los datos en el archivo
+	datos.save("historialCalculosAmbiguedad.cfg")	
+	#_leerCalculosAmbiguedad()	
+func _leerCalculosAmbiguedad():	
+	var datos = ConfigFile.new()
+	var resultado = datos.load("historialCalculosAmbiguedad.cfg")
+	
+	if resultado == OK:  # Si el archivo se cargó correctamente
+		# Obtiene una lista de todas las secciones en el archivo
+		var secciones = datos.get_sections()
+		var json_array = []
+		
+		# Recorre cada sección
+		for seccion in secciones:
+			var velocidadPromedio = datos.get_value(seccion, "velocidadPromedio")
+			var tiempoTotal = datos.get_value(seccion, "tiempoTotal")
+			var destinosCorrectos = datos.get_value(seccion, "destinosCorrectos")
+			var destinosIncorrectos = datos.get_value(seccion, "destinosIncorrectos")
+			
+			# Crea un diccionario para cada sección
+			var dict = {
+				"timestamp": seccion.replace("partida_", ""),
+				"velocidadPromedio": velocidadPromedio,
+				"tiempoTotal": tiempoTotal,
+				"destinosCorrectos": destinosCorrectos,
+				"destinosIncorrectos": destinosIncorrectos
+			}
+			
+			# Agrega el diccionario al array
+			json_array.append(dict)
+		
+		# Convierte el array a JSON
+		var json_text = JSON.stringify(json_array)			
+		#_enviar_datos(json_text, "http://localhost:5000/prediccionw")			
+		var headers = ["Content-Type: application/json"]
+		$HTTPRequest.request("http://localhost:5000/prediccion", headers, HTTPClient.METHOD_POST, json_text)		
+func _enviar_datos(json_text: String, url: String):
+	# Crea una nueva instancia de HTTPRequest
+	var http_request_regresion_lineal = HTTPRequest.new()	
+	# Añade el HTTPRequest al árbol de escena
+	self.add_child(http_request_regresion_lineal)	
+	# Conecta la señal request_completed a una función
+	http_request_regresion_lineal.request_completed.connect( self._on_request_completed)
+	# Prepara los datos de la solicitud
+	var headers = ["Content-Type: application/json"]
+	var body = PackedByteArray(Array(json_text.to_utf8_buffer()))	
+	# Realiza la solicitud POST
+	var error = http_request_regresion_lineal.request(url, headers, HTTPClient.METHOD_POST, body)	
+	if error == OK:
+		print("Solicitud enviada correctamente.")
+	else:
+		print("Error al enviar la solicitud: ", error)
+func _on_request_completed(result, response_code, headers, body):
+	print("Código de respuesta HTTP: ", response_code)
+	var json = JSON.parse_string(body.get_string_from_utf8())
+	print("DEBUG RESPUESTA REGRESION: ",json)
+>>>>>>> Stashed changes
